dfbdb58c7a3f80baa5532c16c81c8d6a85ead83f
Mirbek Idiris uulu <mirba@list.ru>
2024-07-26 10:32:56 +0600

    Update

--- Java.projects/function/ex1.java
+++ Java.projects/function/ex1.java
@@ -8,7 +8,7 @@ public class ex1 {
         Notebook laptop1 = new Notebook(1, "Acer Aspare 3", "Blue", "Windows", "1 Tb", 16);
         Notebook laptop2 = new Notebook(2, " Toshiba Satellite C660-16D", "Black", "Linux", "1 Tb", 32);
         Notebook laptop3 = new Notebook(3, "Lenovo Ideapad 3", "Blue", "DOS", "500 Gb", 8);
-        Notebook laptop4 = new Notebook(3, "Asus Vivobook 15", "Silver", "Ubuntu", "500 Gb", 12);
+        Notebook laptop4 = new Notebook(4, "Asus Vivobook 15", "Silver", "Ubuntu", "500 Gb", 12);
          
         Set<Notebook> laptops = new HashSet<>(Arrays.asList(laptop1, laptop2, laptop3));
         laptops.add(laptop4);
